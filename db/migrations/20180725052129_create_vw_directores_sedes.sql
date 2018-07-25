08784cafefcd02eb45505b9fc52e172a7983446a
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-25 00:37:09 -0500

    Correción en la migración vw_directores_sedes

--- db/migrations/20180725052129_create_vw_directores_sedes.sql
+++ db/migrations/20180725052129_create_vw_directores_sedes.sql
@@ -1,13 +1,14 @@
 -- migrate:up
 
 CREATE VIEW vw_directores_sedes AS SELECT
-  sede_id, nombre1 || ' ' || paterno || ' ' || materno || ', ' || nombres AS director, titulo
-  FROM (
-    SELECT J.sede_id, S.nombre2 AS titulo, S.nombre1, D.nombres, D.paterno, D.materno
-    FROM directores J
-    INNER JOIN doctores D ON D.id= J.doctor_id
-    INNER JOIN sexos S ON D.sexo_id = S.id
-  )
+  sede_id, sede, nombre1 || ' ' || paterno || ' ' || materno || ', ' || nombres AS director, titulo
+    FROM (
+		SELECT J.sede_id, L.nombre AS sede, S.nombre2 AS titulo, S.nombre1, D.nombres, D.paterno, D.materno
+		FROM directores J
+		INNER JOIN doctores D ON D.id= J.doctor_id
+		INNER JOIN sexos S ON D.sexo_id = S.id
+		INNER JOIN sedes L ON D.sede_id = L.id
+	)
 
 -- migrate:down
 
